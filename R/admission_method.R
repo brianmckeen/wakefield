c44e5be2679bc04e796a7e7404fd52c67a6c6b3e
brianmckeen <brianmckeen@users.noreply.github.com>
2015-12-17 11:25:19 +0000

    Correct spelling mistake

--- R/admission_method.R
+++ R/admission_method.R
@@ -1,6 +1,6 @@
 #' Generate Random Vector of Admission Methods
 admission_method <- hijack(r_sample_factor,
-    name = "Admmission Method",
+    name = "Admission Method",
     x = c("Accident and Emergency", "Booked", "Born in Hospital", "Emergency - GP", "Emergency - OP Clinic", "Hospital Other Provider", "Maternity - Ante-Partum", "Maternity - Post-Partum", "Other Emergency Admission", "Other Immediate", "Other Not Immediate", "Planned", "System Generated", "Waiting List"),
     prob = c(.215, .063, .03, .117, .004, .034, .051, .001, .002, .011, .001, .146, .066, .259)
 )
